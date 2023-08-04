fcd58b1231b132d92c317964b8c86a34ae4c56c3
gabrieldevallan <gabriel.contrerasv3@gmail.com>
2023-08-04 17:54:26 -0500

    agregando funcion para contar el espacio que ocupan los videos de una carpeta

---
+++ index.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+const accessToken = process.env.ACCESS_TOKEN;
+
+const project_id = process.env.PROJECT_ID;
+const user_id = process.env.USER_ID;
+
+async function getFolderWeight(project_id) {
+  try {
+    const response = await axios.get(
+      `https://api.vimeo.com/users/${user_id}/projects/${project_id}/videos`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    const data = response.data;
+    const videos = data.data.flatMap((element) =>
+      element.files.map((video) => video.size)
+    );
+    const resultado = videos.reduce((a, b) => a + b, 0);
+
+    const kb = Math.round(resultado / 1024);
+    const mb = Math.round(resultado / 1048576);
+    const gb = Math.round(resultado / 1073741824);
+
+    console.log(resultado + " bytes");
+    console.log(kb + " kb");
+    console.log(mb + " mb");
+    console.log(gb + " gb");
+  } catch (error) {
+    console.error("Error al obtener el peso de la carpeta:", error.message);
+  }
+}
+
+getFolderWeight(project_id);
